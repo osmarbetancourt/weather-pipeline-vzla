bfda87a9b476221a73851d37c445965b5afbfaf2
osmar-dev <oaba.dev@gmail.com>
2025-06-15 23:10:05 -0400

    succesfully loaded API data inside flat json with pandas

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 import requests
 import os
 from dotenv import load_dotenv
+import logging
+import pandas as pd
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 load_dotenv()
 
 # Enviroment variables
@@ -9,6 +12,11 @@ api_key = os.getenv("WEATHER_API_KEY")
 city = os.getenv("TARGET_CITY")
 forecast_days = os.getenv("FORECAST_DAYS")
 api_url = "https://api.weatherapi.com/v1/forecast.json"
+
+if city:
+    logging.info("The enviroment variables were loaded succesfully")
+else:
+    logging.warning("The enviroment variables weren't loaded")
 
 print("Enviroment variables:",city,forecast_days)
 params = {
@@ -20,5 +28,11 @@ params = {
 }
 
 forecast_response = requests.get(url=api_url,params=params)
+forecast_array = forecast_response.json()["forecast"]["forecastday"]
 
-print(forecast_response.json())
+df_hourly_forecast = pd.json_normalize(
+    data=forecast_array,
+    record_path='hour',
+)
+
+print(df_hourly_forecast.columns)
